902cbd1875d29b94feaab1dffecf3cb4ad1bd78e
Ryan Sturmer <ryansturmer@gmail.com>
2018-05-20 13:24:05 -0400

    Snapshot

--- main.js
+++ main.js
@@ -2,22 +2,103 @@ WIDTH = 400
 HEIGHT = 400
 BIT_DIAMETER = 30
 STOCK_DIAMETER = 2
+TURNS = 1
+LENGTH = 6.0
+
+var DEG2RAD = 0.0174533
+var RAD2DEG = 1.0/DEG2RAD
+var TWOPI = 2.0*Math.PI
+
+var scene, camera, renderer;
 
 function write(s) {
   document.getElementById('output').innerHTML += (s + '</br>')
 }
 
-// Setup the display canvas
-var canvas = document.getElementById('cvs')
-canvas.width = WIDTH
-canvas.height = HEIGHT
+function getOptions() {
+	var els = document.getElementsByClassName('opt-num');
+	options = {}
+	for(var i=0; i<els.length; i++){
+		options[els[i].id.replace(/^(option-)/,"")] = parseFloat(els[i].value);
+	}
+	return options;
+}
 
-// Draw the profile on the canvas
-var ctx = canvas.getContext('2d')
-//ctx.moveTo(center.x, center.y-30)
-//ctx.arc(center.x, center.y-30, 20, 0, TWOPI)
-//ctx.moveTo(center.x, center.y)
-//ctx.arc(center.x, center.y, 20, 0, TWOPI)
+function setupUI() {
+	var tabs = document.getElementsByClassName("view-tab");
+		console.log(tabs);
+		for(var i=0; i<tabs.length; i++) {
+			var tab = tabs[i];
+			tab.addEventListener('click', function(evt) {
+				[].forEach.call(document.querySelectorAll('.view-pane'), function (el) {
+				  el.style.display = 'none';
+				});
+				console.log(evt.target.dataset)
+				var el = document.getElementById(evt.target.dataset.target);
+				el.style.display = 'block'
+			});
+		}
+}
+
+function makePart(options) {
+	var canvas = document.getElementById('cvs')
+	canvas.width = WIDTH
+	canvas.height = HEIGHT
+	var center = {
+		x : canvas.width/2,
+		y : canvas.height/2
+	}
+	// Draw the profile on the canvas
+	var ctx = canvas.getContext('2d')
+	//ctx.moveTo(center.x, center.y-30)
+	ctx.arc(center.x-18, center.y, 20, 0, TWOPI)
+	//ctx.moveTo(center.x, center.y+30)
+	ctx.arc(center.x+18, center.y, 20, 0, TWOPI)
+	ctx.fill()
+
+	/*
+	ctx.beginPath()
+	ctx.fillRect(center.x-5, center.y-40, 10, 80)
+	ctx.fillRect(center.x-40, center.y-5, 80, 10)
+	ctx.arc(center.x, center.y, 15, 0, TWOPI)
+	ctx.fill()
+*/
+/*
+	R = 50
+	ctx.beginPath()
+	ctx.moveTo(center.x + R*Math.sin(DEG2RAD*120.0), center.y + R*Math.cos(DEG2RAD*120.0))
+	ctx.arc(center.x + R*Math.sin(DEG2RAD*120.0), center.y + R*Math.cos(DEG2RAD*120.0), 10, 0, TWOPI)
+
+	ctx.moveTo(center.x + R*Math.sin(DEG2RAD*240.0), center.y + R*Math.cos(DEG2RAD*240.0))
+	ctx.arc(center.x + R*Math.sin(DEG2RAD*240.0), center.y + R*Math.cos(DEG2RAD*240.0), 10, 0, TWOPI)
+	ctx.fill()
+
+	ctx.moveTo(center.x + R*Math.sin(DEG2RAD*0), center.y + R*Math.cos(DEG2RAD*0))
+	ctx.arc(center.x + R*Math.sin(DEG2RAD*0), center.y + R*Math.cos(DEG2RAD*0), 10, 0, TWOPI)
+	ctx.fill()
+*/
+	var t = new TurningMachine(canvas, {
+	  bitDiameter : 0.125,
+	  stockDiameter : 1.15,
+	  length : LENGTH,
+	  turns : TURNS
+	});
+
+
+	t.render(document.getElementById('bitcvs'))
+	make3DModel(t);
+}
+
+//function makeHelix(options) {
+	var options = options || {}
+	var strands = options['strands'] || 2;
+
+	// Setup the display canvas
+
+//}
+
+
+
 //ctx.moveTo(center.x, center.y+30)
 //ctx.arc(center.x, center.y+30, 20, 0, TWOPI)
 //ctx.fillRect(center.x-40, center.y-40, 80, 80)
@@ -30,20 +111,20 @@ ctx.arc(center.x, center.y, 15, 0, TWOPI)
 ctx.fill()
 */
 
-var center = {x:canvas.width/2, y:canvas.height/2}
-
+/*
+R = 50
 ctx.beginPath()
-ctx.moveTo(center.x + 30*Math.sin(DEG2RAD*120.0), center.y + 30*Math.cos(DEG2RAD*120.0))
-ctx.arc(center.x + 30*Math.sin(DEG2RAD*120.0), center.y + 30*Math.cos(DEG2RAD*120.0), 10, 0, TWOPI)
+ctx.moveTo(center.x + R*Math.sin(DEG2RAD*120.0), center.y + R*Math.cos(DEG2RAD*120.0))
+ctx.arc(center.x + R*Math.sin(DEG2RAD*120.0), center.y + R*Math.cos(DEG2RAD*120.0), 10, 0, TWOPI)
 
-ctx.moveTo(center.x + 30*Math.sin(DEG2RAD*240.0), center.y + 30*Math.cos(DEG2RAD*240.0))
-ctx.arc(center.x + 30*Math.sin(DEG2RAD*240.0), center.y + 30*Math.cos(DEG2RAD*240.0), 10, 0, TWOPI)
+ctx.moveTo(center.x + R*Math.sin(DEG2RAD*240.0), center.y + R*Math.cos(DEG2RAD*240.0))
+ctx.arc(center.x + R*Math.sin(DEG2RAD*240.0), center.y + R*Math.cos(DEG2RAD*240.0), 10, 0, TWOPI)
 ctx.fill()
 
-
-ctx.moveTo(center.x + 30*Math.sin(DEG2RAD*0), center.y + 30*Math.cos(DEG2RAD*0))
-ctx.arc(center.x + 30*Math.sin(DEG2RAD*0), center.y + 30*Math.cos(DEG2RAD*0), 10, 0, TWOPI)
+ctx.moveTo(center.x + R*Math.sin(DEG2RAD*0), center.y + R*Math.cos(DEG2RAD*0))
+ctx.arc(center.x + R*Math.sin(DEG2RAD*0), center.y + R*Math.cos(DEG2RAD*0), 10, 0, TWOPI)
 ctx.fill()
+*/
 
 /*ctx.beginPath()
 ctx.fillStyle = 'black'
@@ -66,13 +147,121 @@ ctx.arc(center.x, center.y, 10, 0, TWOPI)
 ctx.fill()
 */
 
-var t = new TurningMachine(canvas, {
-  bitDiameter : 0.125,
-  stockDiameter : 1.15,
-  length : 6.0
+function setup3DView() {
+	var container = document.getElementById('canvas3d');
+	
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, container.offsetWidth/container.offsetHeight, 0.1, 1000 );
+	renderer = new THREE.WebGLRenderer({antialias : true});
+	renderer.setClearColor( 0xffffff, 1 );
 
+	var ambientLight = new THREE.AmbientLight( 0x404040, 1.0); // soft white light
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 2.0 );
+	var pointLight = new THREE.PointLight( 0xffffff, 2, 100 );
+
+	camera.add(pointLight)
+
+	scene.add(pointLight)
+	scene.add(ambientLight);
+	scene.add(directionalLight);
+
+
+	//var controls = new THREE.OrbitControls( camera );
+
+	renderer.setSize( container.offsetWidth, container.offsetHeight );
+	container.appendChild( renderer.domElement );
+
+
+}
+
+function make3DModel(t) {
+	var points = t.getBoundaryPoints()
+	var points3d = []
+
+	var STEPS = 100
+	var dt = TWOPI*TURNS/STEPS
+	var dl = t.length/STEPS
+
+	function rotate(pt, t) {
+		return {
+			x : pt.x*Math.cos(t) - pt.y*Math.sin(t),
+			y : pt.y*Math.cos(t) + pt.x*Math.sin(t)
+		}
+	}
+
+	for(var i =0; i<STEPS; i++) {
+		points.forEach(function(point) {
+			var p = rotate(point, dt*i)
+			var z = i*dl
+			points3d.push(new THREE.Vector3(p.x,p.y,z));
+		});
+	}
+
+	var geom = new THREE.Geometry(); 
+
+
+	points3d.forEach(function(point) {
+		geom.vertices.push(point)
+	});
+
+	for(var s=0; s<STEPS-1; s++) {
+		for(var i=0; i<points.length-1; i++) {
+			a = (s*points.length) + i
+			b = (s*points.length) + i+1
+			c = (s+1)*points.length + i
+			d = (s+1)*points.length + i+1
+			geom.faces.push(new THREE.Face3(b,a,c))	
+			geom.faces.push(new THREE.Face3(c,d,b))	
+		}
+	}
+	geom.computeFaceNormals()
+	geom.translate(0,0,-t.length/2.0)
+
+
+	var pointsMaterial = new THREE.PointsMaterial( { color: 0x888888, size : 0.05 } );
+	var pointsObject = new THREE.Points( geom, pointsMaterial );
+
+	//var meshMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true } );
+	var meshMaterial = new THREE.MeshLambertMaterial({ color: 0xc6aa79, side:THREE.DoubleSide})
+	var meshObject = new THREE.Mesh(geom, meshMaterial);
+
+	scene.add(meshObject);
+	//scene.add(pointsObject);
+
+	var object = meshObject;
+	object.rotation.x = TWOPI/4
+	var animate = function () {
+	        requestAnimationFrame( animate );
+
+	        object.rotation.x += 0.01;
+	        object.rotation.y += 0.01;
+
+	        renderer.render(scene, camera);
+	};
+
+	function animateControls() {
+
+		requestAnimationFrame( animateControls );
+
+		// required if controls.enableDamping or controls.autoRotate are set to true
+		controls.update();
+
+		renderer.render( scene, camera );
+
+	}
+
+	renderer.render(scene, camera)
+	camera.position.z = t.length*0.75;
+
+	//animateControls();
+	animate()
+
+}
+
+setupUI();
+setup3DView();
+
+document.getElementById('btn-update').addEventListener('click', function() {
+	makePart();
 });
-
-
-t.render(document.getElementById('bitcvs'))
-write(t.postSBP().join('<br />'))
+makePart({});
--- toolpath.js
+++ toolpath.js
@@ -1,6 +1,7 @@
-var DEG2RAD = 0.0174533
-var RAD2DEG = 1.0/DEG2RAD
-var TWOPI = 2.0*Math.PI
+
+var DEG2RAD = 0.0174533;
+var RAD2DEG = 1.0/DEG2RAD;
+var TWOPI = 2.0*Math.PI;
 
 function TurningMachine(sourceCanvas, options) {
   var options = options || {};
@@ -34,6 +35,11 @@ TurningMachine.prototype._testCut = function(bitDiameterPx, depth) {
   bitCanvas.height = this.sourceCanvas.height/2
   var bitContext = bitCanvas.getContext('2d')
   
+  var center = {
+    x : this.sourceCanvas.width/2,
+    y : this.sourceCanvas.height/2
+  }
+
   // Draw the bit silhouette
   bitContext.globalCompositeOperation = 'source-over'
   if(depth > bitDiameterPx/2) {
@@ -49,7 +55,7 @@ TurningMachine.prototype._testCut = function(bitDiameterPx, depth) {
   // Test for *any* pixels in the image.  If so, there is overlap, and the bit is in the material
   var imgData = bitContext.getImageData(0,0, bitDiameterPx, depth);
   for(var i=3; i<imgData.data.length; i+=4) {
-    if(imgData.data[i] != 0) {
+    if(imgData.data[i] !== 0) {
       return true;
     }
   }
@@ -61,13 +67,13 @@ TurningMachine.prototype._testCut = function(bitDiameterPx, depth) {
 TurningMachine.prototype._getSourceDiameter = function() {
   //console.log(this._center)
 	var ctx = this.sourceCanvas.getContext('2d');
-  var imgData = ctx.getImageData(0,0,cvs.width, cvs.height);
+  var imgData = ctx.getImageData(0,0,this.sourceCanvas.width, this.sourceCanvas.height);
   var radius = 0;
   for(var i=3; i<imgData.data.length; i+=4) {
-  	if(imgData.data[i] != 0) {
-    	pos = (i-4);
-      var x = (pos / 4) % cvs.width;
-			var y = Math.floor((pos / 4) / cvs.width);
+  	if(imgData.data[i] !== 0) {
+    	var pos = (i-4);
+      var x = (pos / 4) % this.sourceCanvas.width;
+			var y = Math.floor((pos / 4) / this.sourceCanvas.width);
       var dist = Math.sqrt((x-this._center.x)*(x-this._center.x) + (y-this._center.y)*(y-this._center.y));
     	if(dist > radius) {
       	radius = dist;
@@ -84,7 +90,7 @@ TurningMachine.prototype._findDepth = function(bitDiameter) {
   // Binary search for a bit hit
   while(true) {
     if(l > r) {
-      return (this.sourceCanvas.height/2) - (m-1)
+      return (this.sourceCanvas.height/2 - (m-1))
     }
     var m = Math.floor((l+r)/2)
     if(this._testCut(bitDiameter, m)) {
@@ -97,20 +103,26 @@ TurningMachine.prototype._findDepth = function(bitDiameter) {
 
 TurningMachine.prototype._computeCrossSectionToolpath = function() {
 
-  // Create a memory canvas and copy off the profile
+  // Create a memory canvas and copy off the cross section
   var memoryCanvas = document.createElement('canvas')
   memoryCanvas.width = this.sourceCanvas.width
   memoryCanvas.height = this.sourceCanvas.height
   memoryCanvas.getContext("2d").drawImage(this.sourceCanvas, 0, 0, this.sourceCanvas.width, this.sourceCanvas.height)
 
+  var ctx = memoryCanvas.getContext("2d");
   var result = []
   var angle = 0;
   var as = 0.5;
+  console.log("Center", this._center)
+
   while(angle < 360) {
+    var rot = angle*DEG2RAD;
     ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0,0,WIDTH,HEIGHT)
+    ctx.clearRect(0,0,this.sourceCanvas.width,this.sourceCanvas.height)
+
+    //Translate to the origin, rotate, translate back
     ctx.translate(this._center.x,this._center.y)
-    ctx.rotate(angle*DEG2RAD);
+    ctx.rotate(rot);
     ctx.translate(-this._center.x,-this._center.y)
     ctx.drawImage(memoryCanvas,0,0,this.sourceCanvas.width, this.sourceCanvas.height)
     result.push({theta:angle*DEG2RAD, r:this._findDepth(this._bitDiameterPx)/this._pixelsPerInch})
@@ -171,15 +183,18 @@ TurningMachine.prototype.postSBP = function(options) {
 }
 
 TurningMachine.prototype.render = function(canvas) {
+
+  canvas = canvas || document.createElement('canvas')
   canvas.width = this.sourceCanvas.width
   canvas.height = this.sourceCanvas.height 
 
   var ctx = canvas.getContext("2d");
 
-  theta = 0
+  var theta = 0
   while(theta < TWOPI) {
     var depth = this._pixelsPerInch*getDepth(this.crossSection, theta);
-    console.log(depth)
+    //console.log(depth)
+    ctx.fillStyle = 'rgba(0,0,64,255)'
     ctx.fillRect(this._center.x-this._bitDiameterPx/2,
       this._center.y + depth + this._bitDiameterPx/2,this._bitDiameterPx,2*this._center.y)
     ctx.beginPath()
@@ -190,4 +205,78 @@ TurningMachine.prototype.render = function(canvas) {
     ctx.translate(-this._center.x,-this._center.y)
     theta += 0.5*DEG2RAD;
   }
+  return canvas
+}
+
+TurningMachine.prototype.getBoundaryPoints = function(canvas) {
+  var cvs = this.render(canvas);
+  //console.log(cvs)
+  var ctx = cvs.getContext('2d')
+  var imgData = ctx.getImageData(0,0,cvs.width, cvs.height);
+  var result = {}
+  //var pxCount = 0
+  var startPoint = null;
+  //console.log("imgdata length", imgData.data.length)
+  for(var x = 1; x<cvs.width-1; x++) {
+    for(var y = 1; y<cvs.height-1; y++) {
+        var aidx = y * (cvs.width * 4) + x * 4 + 3    
+        var neighbors = [
+            y * (cvs.width * 4) + (x-1) * 4 + 3,
+            y * (cvs.width * 4) + (x+1) * 4 + 3,
+            (y+1) * (cvs.width * 4) + (x) * 4 + 3,
+            (y-1) * (cvs.width * 4) + (x) * 4 + 3
+        ]
+        if(imgData.data[aidx] !== 0) {
+          var hit = false;
+          for(var j=0; j<4; j++) {
+            if(imgData.data[neighbors[j]] === 0) {
+              hit = true;
+            }
+          }
+          if(hit) { 
+            if(!startPoint) {
+              startPoint = [x,y];
+            }
+            result[[x,y]] = 1;
+          }
+        }
+    }
+  }
+
+  var currentPoint = startPoint;
+  var seen = {}
+  var ordered_points = []
+  while(true) {
+    ordered_points.push(currentPoint);
+    var neighbors = [
+      [currentPoint[0]+1, currentPoint[1]],
+      [currentPoint[0]-1, currentPoint[1]],
+      [currentPoint[0], currentPoint[1]+1],
+      [currentPoint[0], currentPoint[1]-1],
+      [currentPoint[0]+1, currentPoint[1]+1],
+      [currentPoint[0]-1, currentPoint[1]-1],
+      [currentPoint[0]+1, currentPoint[1]-1],
+      [currentPoint[0]-1, currentPoint[1]+1],
+    ]
+    var hit = false
+    for(var i=0; i<8; i++) {
+      if(result[neighbors[i]]) {
+        //ordered_points neighbors[i];
+        delete result[neighbors[i]];
+        currentPoint = neighbors[i];
+        hit = true;
+        break;
+      }
+    }
+    if(!hit) {break;}
+  }
+  //console.log(ordered_points.length + " Ordered points");
+  var sorted =  ordered_points.map(function(pt) { 
+    return { 
+      x : (pt[0]-cvs.width/2)/this._pixelsPerInch,
+      y : (pt[1]-cvs.height/2)/this._pixelsPerInch
+    }
+  }.bind(this));
+  //console.log(sorted)
+  return sorted
 }
